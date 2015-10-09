89532c3d00b64d230839c696b847b61f784b20c4
linxihui <xihuil.silence@gmail.com>
2015-10-09 10:29:55 -0400

    added tmux conf

--- some_useful_scripts/sublime_install_sendtext_to_terminal.sh
+++ some_useful_scripts/sublime_install_sendtext_to_terminal.sh
@@ -6,9 +6,21 @@
 sub=`ls $HOME/.config/ | grep sublime`
 tmuxloc=`which tmux`
 
+if [ ! $tmuxloc ]
+then
+	sudo apt-get -y install tmux
+	tmuxloc=`which tmux`
+fi
+
+if [ ! -f $HOME/.tmux.conf ]
+then
+	wget -q https://raw.githubusercontent.com/linxihui/Misc/master/some_useful_scripts/tmux.conf -O $HOME/.tmux.conf
+fi
+
 if [ ! -d $ubs ]
 then
 	echo "Sublime Text not installed."
+	exit 1
 fi
 
 if [ -d $HOME/.config/$sub/SendText ]
@@ -21,11 +33,6 @@ unzip -q /tmp/_sublime_sendtext/master.zip -d /tmp/_sublime_sendtext
 
 cp -R /tmp/_sublime_sendtext/SendText-master ~/.config/$sub/Packages/SendText
 rm -rf /tmp/_sublime_sendtext
-
-if [ ! $tmuxloc ]
-then
-	sudo apt-get -y install tmux
-fi
 
 echo "{
     // Uncomment the program you want send text to:
