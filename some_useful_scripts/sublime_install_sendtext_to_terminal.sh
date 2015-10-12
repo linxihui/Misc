cda297f50de73139207e9419361b1879beaf08b4
linxihui <xihuil.silence@gmail.com>
2015-10-12 16:44:26 -0400

    fixed a typo: $ubs->$sub; added commends

--- some_useful_scripts/sublime_install_sendtext_to_terminal.sh
+++ some_useful_scripts/sublime_install_sendtext_to_terminal.sh
@@ -3,37 +3,43 @@
 # Eric Xihui Lin, Oct 08 2015
 # Work on Linux only
 
+# get sublime version and tmux location
 sub=`ls $HOME/.config/ | grep sublime`
 tmuxloc=`which tmux`
 
+# check if sublime installed
+if [ ! -d $sub ]
+then
+	echo "Sublime Text not installed."
+	exit 1
+fi
+
+# install tmux if not exists
 if [ ! $tmuxloc ]
 then
 	sudo apt-get -y install tmux
 	tmuxloc=`which tmux`
 fi
 
+# config tmux
 if [ ! -f $HOME/.tmux.conf ]
 then
 	wget -q https://raw.githubusercontent.com/linxihui/Misc/master/some_useful_scripts/tmux.conf -O $HOME/.tmux.conf
 fi
 
-if [ ! -d $ubs ]
-then
-	echo "Sublime Text not installed."
-	exit 1
-fi
-
-if [ -d $HOME/.config/$sub/SendText ]
+# check if plugin 'SendText' has been installed
+if [ -d $HOME/.config/$sub/Packages/SendText ]
 then
 	exit 0
 fi
 
+# get SendText, unzip, move to sublime package location, and clean downloads
 wget -q https://github.com/wch/SendText/archive/master.zip -P /tmp/_sublime_sendtext
 unzip -q /tmp/_sublime_sendtext/master.zip -d /tmp/_sublime_sendtext
-
 cp -R /tmp/_sublime_sendtext/SendText-master ~/.config/$sub/Packages/SendText
 rm -rf /tmp/_sublime_sendtext
 
+# config SendText: use tmux
 echo "{
     // Uncomment the program you want send text to:
     // \"program\": \"Terminal.app\",
