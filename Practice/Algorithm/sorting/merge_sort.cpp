07c742df777a3d3f81ae13be53393b04e38f3e56
linxihui <xihuil.silence@gmail.com>
2015-11-16 22:50:24 -0500

    changed name

--- Practice/Algorithm/sorting/merge_sort.cpp
+++ Practice/Algorithm/sorting/merge_sort.cpp
@@ -1,11 +1,16 @@
-#include <cmath>
+/* TODO:
+ * 1. parallel
+ * 2. merge sort on linked list
+ * 3. optimize the usage of auxiliary
+ */
+
 #include <bits/stdc++.h>
 #include <armadillo>
 
 using namespace std;
 
 template <typename T>
-int merge_sort(vector<T> & x, int start, int end)
+void mergesort(vector<T> & x, int start, int end)
 {
 	if (start == end) return 1;
 	int n = end - start + 1;
@@ -13,6 +18,7 @@ int merge_sort(vector<T> & x, int start, int end)
 	merge_sort(x, start, start + n1-1);
 	merge_sort(x, start + n1, end);
 
+	// temp array for mergeing
 	vector<T> x2(n);
 	for (int ii = 0; ii < n; ii ++)
 		x2[ii] = x[start+ii];
@@ -20,6 +26,7 @@ int merge_sort(vector<T> & x, int start, int end)
 	int i = 0, j = n1;
 	typename vector<T>::iterator k = x.begin()+start;
 
+	// merge the left and right sorted array
 	while(i < n1 && j < n)
 	{
 		if (x2[i] <= x2[j])
@@ -36,26 +43,29 @@ int merge_sort(vector<T> & x, int start, int end)
 
 	for (; i < n1; i++, k++) *k = x2[i];
 	for (; j < n; j++, k++) *k = x2[j];
+}
 
-	return 1;
+template <typename T>
+void mergesort(vector<T> & x)
+{
+	mergesort(x, 0, x.size()-1);
 }
 
 
 int main(int argc, char* argv[]) 
 {
 	int n = 10;
-	if (argc >= 2) 
-		n = atoi(argv[1]);
+	if (argc >= 2) n = atoi(argv[1]);
 	cout << "n = " << n << endl;
 	arma::vec x0 = arma::randu<arma::vec>(n);
 	vector<double> x(x0.begin(), x0.end());
 
-	merge_sort(x, 0, n-1);
+	mergesort(x);
 
 	bool sorted = true;
-	for (int i = 1; i < n; i++)
+	for (int i = 1; sorted &&i < n; i++)
 	{
-		sorted = (x[i] - x[i-1] >= 0)  && sorted;
+		sorted = (x[i] - x[i-1] >= 0);
 	}
 
 	if (sorted)
