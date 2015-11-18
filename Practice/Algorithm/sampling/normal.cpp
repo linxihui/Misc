6f487b261022a7aad2a44c6daf4d31cd430ab4a5
linxihui <xihuil.silence@gmail.com>
2015-11-17 23:03:44 -0500

    generate normal sample

---
+++ Practice/Algorithm/sampling/normal.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+#include <omp.h>
+
+using namespace std;
+
+vector<double> rand_std_norm(int n)
+{
+	// generate standard normal distribution using Box-Muller transform
+	// if u1, u2 ~ unif(0,1) independently, then 
+	// r1 = sqrt(-2*log(u1)) * cos(2*M_PI*u2)
+	// r2 = sqrt(-2*log(u1))*sin(2*M_PI*u2)
+	// are independent standardard normal sample
+	double u1, u2;
+	vector<double> out(n);
+	double a, b;
+	vector<double>::iterator p = out.begin();
+	//pragma omp parallel for
+	for (int j = 0; j < n/2; j++)
+	{
+		u1 = double(rand()) / RAND_MAX;
+		u2 = double(rand()) / RAND_MAX;
+		a = ::sqrt(-2*::log(u1));
+		b = ::cos(2*M_PI*u2);
+		*p++ = a*b;
+		*p++ = (u2 > 0.5) ? a*::sqrt(1.-b*b) : -a*::sqrt(1.-b*b);
+	}
+
+	if ( n % 2 == 1)
+	{
+		u1 = double(rand()) / RAND_MAX;
+		u2 = double(rand()) / RAND_MAX;
+		*p = ::sqrt(-2*::log(u1)) * ::cos(2*M_PI*u2);
+	}
+
+	return out;
+}
+
+int main() {
+
+	int n = 20;
+	vector<double> x(n);
+
+	x = rand_std_norm(n);
+
+	for (vector<double>::iterator p = x.begin(); p < x.end(); p++)
+	{
+		cout << *p << endl;
+	}
+	return 0;
+}
