b95591f4efc1cc800ee6c5fd02deeb746b11625f
linxihui <xihuil.silence@gmail.com>
2015-11-20 17:14:31 -0500

    added L2 constraint

--- Practice/NMF/seq_quad_KL.cpp
+++ Practice/NMF/seq_quad_KL.cpp
@@ -8,7 +8,7 @@ using namespace arma;
 vec sqKL(mat A, mat b, double eta = 0, int max_iter = 10000, double rel_tol = 1e-8)
 {
 	// solve Non-negative linear equation by minimizing KL-divergence distance, i.e.,
-	// 		argmin_{x>=0} || Ax - b log(Ax)||_F^2 + eta/2 ||x||_F^2
+	// 		argmin_{x>=0} sum(Ax - b log(Ax)) + eta/2 ||x||_F^2
 	// Agorithm:
 	// 	 Sequentially update each entry of x by approximating the KL-divergence distance with
 	// 	 a quadratic function, which can be solved explicitely. This is equivalent to sequential
@@ -18,8 +18,9 @@ vec sqKL(mat A, mat b, double eta = 0, int max_iter = 10000, double rel_tol = 1e
 	vec x0;
 	vec b0 = A*x;
 	double alpha, beta;
-	double err = 0, err0 = 9999;
+	double err = 9999, err0;
 	double rel_err = rel_tol + 1;
+	double tmp;
 	
 	for (int t = 0; t < max_iter && rel_err > rel_tol; t++)
 	{
@@ -27,12 +28,12 @@ vec sqKL(mat A, mat b, double eta = 0, int max_iter = 10000, double rel_tol = 1e
 		err0 = err;
 		for (int k = 0; k < A.n_cols; k++)
 		{
-			b0 = b / b0;
-			alpha = eta + sum(b0 % square(A.col(k)));
-			beta = sum(A.col(k) % (1 - b0));
+			alpha = eta + sum((b/b0) % square(A.col(k)));
+			beta = sum(A.col(k) % (1 - b/b0));
+			tmp = x(k);
 			x(k) -= beta / (2*alpha); 
 			if (x(k) < 0) x(k) = 0;
-			b0 = A*x; // optimize here!
+			b0 += (x(k) - tmp) * A.col(k);
 		}
 		//err = sum(b0 - b % trunc_log(b0));
 		err = max(abs(x - x0));
